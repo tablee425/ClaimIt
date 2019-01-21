90d276628f65a2675c38fe58697fa4e2f736da31
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2019-01-21 19:17:56 +0400

    android splash screen

---
+++ ClaimIt/:Volumes:MacData:Works:Xamarin:Test:Source:ClaimIt:ClaimIt:ClaimIt.Android:Main2Activity.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ClaimIt.Droid
+{
+  [Activity(Label = "@string/ApplicationName")]
+  public class MainActivity : AppCompatActivity
+  {
+    static readonly string TAG = "X:" + typeof(MainActivity).Name;
+    Button _button;
+    int _clickCount;
+
+    protected override void OnCreate(Bundle bundle)
+    {
+      base.OnCreate(bundle);
+      SetContentView(Resource.Layout.Layout);
+
+      _button = FindViewById<Button>(Resource.Id.MyButton);
+
+      _button.Click += (sender, args) =>
+      {
+        string message = string.Format("You clicked {0} times.", ++_clickCount);
+        _button.Text = message;
+        Log.Debug(TAG, message);
+      };
+
+      Log.Debug(TAG, "MainActivity is loaded.");
+    }
+  }
+}
--- ClaimIt/ClaimIt.Android/MainActivity.cs
+++ ClaimIt/ClaimIt.Android/MainActivity.cs
@@ -1,15 +1,13 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Widget;
 
 namespace ClaimIt.Droid
 {
-  [Activity(Label = "ClaimIt", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+  [Activity(Icon = "@mipmap/icon", Theme = "@style/MainTheme")]
   public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
   {
     protected override void OnCreate(Bundle savedInstanceState)
