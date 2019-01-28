0d7c916f19c6d5f07871f2a1a8b28f37d644d669
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2019-01-28 21:14:02 +0400

    add mac os version

---
+++ ClaimIt/ClaimIt.MacOS/AppDelegate.cs
@@ -0,0 +1,40 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace ClaimIt.MacOS
+{
+  [Register("AppDelegate")]
+  public class AppDelegate : FormsApplicationDelegate
+  {
+    NSWindow _window;
+    public AppDelegate()
+    {
+      var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+      var rect = new CoreGraphics.CGRect(200, 200, 800, 600);
+      _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+    }
+
+    public override NSWindow MainWindow
+    {
+      get { return _window; }
+    }
+
+    public override void DidFinishLaunching(NSNotification notification)
+    {
+      // Insert code here to initialize your application
+      Forms.Init();
+      LoadApplication(new App());
+      base.DidFinishLaunching(notification);
+      _window.StyleMask = _window.StyleMask | NSWindowStyle.Titled;
+      _window.Title = "ClaimIt";
+    }
+
+    public override void WillTerminate(NSNotification notification)
+    {
+      // Insert code here to tear down your application
+    }
+  }
+}
